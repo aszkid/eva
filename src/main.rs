3bf44f2ebd9c19b96cf468acbdf645bf6933fb61
Pol Gómez Riquelme <gomezp@uchicago.edu>
2020-05-16 17:16:04 +0200

    Cleaner code

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,20 @@ struct Service {
     vars: Vec<(String, String)>,
 }
 
+fn extract_envvars<I, K, V>(iter: I, re: &regex::Regex) -> Vec<(String, String)>
+where I: Iterator<Item = (K, V)>,
+      K: AsRef<str>,
+      V: AsRef<str> {
+    iter.filter_map(|(k, v)| {
+        re.captures(k.as_ref()).map(|caps| (caps[1].to_string(), v.as_ref().to_string()))
+    }).collect()
+}
+
 async fn run_svc(svc_name: &str, svc: &Service) {
     info!("spawning `{}`...", svc_name);
 
     let re = regex::Regex::new(&format!(r"(?:EVA__{}__)([\w|_]+)", svc_name)).unwrap();
-    let vars: Vec<(String, String)> = std::env::vars().filter_map(|(k,v)| {
-        re.captures(&k).map(|caps| (caps[1].to_string(), v.to_string()))
-    }).collect();
+    let vars: Vec<(String, String)> = extract_envvars(std::env::vars(), &re);
 
     let mut cmd = Command::new(&svc.exec)
         //.env_clear()
@@ -37,12 +44,6 @@ async fn run_svc(svc_name: &str, svc: &Service) {
 
     let exit = cmd.await.expect("failed to wait for process");
     info!("final {:?}", exit);
-}
-
-fn extract_envvars(props: &ini::ini::Properties, re: &regex::Regex) -> Vec<(String, String)> {
-    props.iter().filter_map(|(k, v)| {
-        re.captures(&k).map(|caps| (caps[1].to_string(), v.to_string()))
-    }).collect()
 }
 
 #[tokio::main]
@@ -66,23 +67,20 @@ async fn main() -> Result<(), String> {
         info!("loading service `{}`...", &svc_name);
 
         if let Some(exec) = prop.get("exec") {
-            let mut svc = Service {
+            let svc = Service {
                 exec: exec.to_string(),
-                vars: extract_envvars(prop, &re)
+                vars: extract_envvars(prop.iter(), &re)
             };
-            
             services.insert(svc_name, svc);
             info!("   ok!");
         } else {
             error!("   skipped, no executable!");
         }
     }
-
-    let mut futs = Vec::new();
-    for (svc, exec) in services.iter() {
-        futs.push(run_svc(svc, exec));
-    }
-
+    
+    let futs = services.iter().map(|(svc, exec)| {
+        run_svc(svc, exec)
+    });
     futures::future::join_all(futs).await;
 
     Ok(())
